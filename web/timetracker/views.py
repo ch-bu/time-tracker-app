0cfe5591fee0423536ab9d13d7971a15ea981e0b
Christian Burkhart <burkhart.christian@gmail.com>
2017-01-22 13:35:46 +0100

    serve model data to index

--- web/timetracker/views.py
+++ web/timetracker/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from htmlmin.decorators import minified_response
+from timetracker.models import Work
 
 # Create your views here.
 #
 @minified_response
 def index(request):
-    return render(request, 'mysite/base.html')
+    # Get all work objects
+    work = Work.objects.all()
+
+    # Pick get comment of first work object
+    comment = work[0].started
+    return render(request, 'mysite/base.html', {'comment': comment})
