028531ad04b91ee762afbd73f48762e0c946dec8
Christian Burkhart <burkhart.christian@gmail.com>
2017-01-22 13:23:11 +0100

    add model for work

--- web/timetracker/models.py
+++ web/timetracker/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Work(models.Model):
+    work_category = models.CharField(max_length=300)
+    work_type = models.CharField(max_length=300)
+    started = models.DateTimeField()
+    stopped = models.DateTimeField()
+    goal = models.TextField()
+    comment = models.TextField()
+    feeling = models.PositiveSmallIntegerField()
