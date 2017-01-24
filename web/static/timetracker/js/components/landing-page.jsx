87ee39477ad02b327d130f970d807490d50d841a
Christian Burkhart <burkhart.christian@gmail.com>
2017-01-24 22:42:26 +0100

    add static files

---
+++ web/static/timetracker/js/components/landing-page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './header.jsx';
+
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <Header />
+    )
+  }
+}
+
+export default LandingPage;
