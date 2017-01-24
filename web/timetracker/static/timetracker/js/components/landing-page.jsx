53e826ecac4fb89c1b9611adb44f858f6a1a7ad2
Christian Burkhart <burkhart.christian@gmail.com>
2017-01-24 22:12:21 +0100

    create first react component

---
+++ web/timetracker/static/timetracker/js/components/landing-page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <h1>Hello, this is my react component</h1>
+    )
+  }
+}
+
+export default LandingPage;
