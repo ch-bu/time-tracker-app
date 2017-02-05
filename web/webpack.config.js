18c2ec3a33e89f5217933b3b30d81458e9254298
Christian Burkhart <burkhart.christian@gmail.com>
2017-02-05 20:15:24 +0100

    strip webpack to minimum

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -40,11 +40,11 @@ module.exports = {
   },
 
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
+    // new webpack.DefinePlugin({
+    //   'process.env': {
+    //     NODE_ENV: JSON.stringify('production')
+    //   }
+    // }),
 
     new webpack.optimize.UglifyJsPlugin({
       compress: {
@@ -55,9 +55,9 @@ module.exports = {
       comments: false
     }),
 
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
-    })
+    // new webpack.ProvidePlugin({
+    //   $: 'jquery',
+    //   jQuery: 'jquery'
+    // })
   ]
 };
