4b68c41f71a161e72b6adbdf6802f0342156a8ec
Christian Burkhart <burkhart.christian@gmail.com>
2017-02-14 19:06:49 +0100

    style: Improved visual appearence of login page.
    Removed color in the background  and made login responsive.

--- web/static/timetracker/js/components/login.jsx
+++ web/static/timetracker/js/components/login.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 class Login extends React.Component {
   constructor(props) {
     super(props);
--- web/timetracker/static/timetracker/js/components/login.jsx
+++ web/timetracker/static/timetracker/js/components/login.jsx
@@ -9,17 +9,21 @@ class Login extends React.Component {
         <form className="col s12">
 
           <div className="row">
-            <div className="input-field col s10">
+            <div className="input-field col s12">
               <input id="username" type="text" />
               <label htmlFor="username">Username</label>
             </div>
           </div>
 
           <div className="row">
-            <div className="input-field col s10">
+            <div className="input-field col s12">
               <input id="password" type="password" />
               <label htmlFor="password">Password</label>
             </div>
+          </div>
+
+          <div className="row">
+            <a id="login-button" className="waves-effect waves-light btn">button</a>
           </div>
 
         </form>
