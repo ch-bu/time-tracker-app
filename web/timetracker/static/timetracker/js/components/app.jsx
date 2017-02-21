865b39d426a2ccbe84c42efd29437e1c1bfd2688
Christian Burkhart <burkhart.christian@gmail.com>
2017-02-21 19:17:06 +0100

    feat: Start task input on app
    Users should be able to start a task very easily. I therefore
    put the input box at the top of the page. Users are now able
    to write down on what they are working on. The react
    component saves the state of the input box.

--- web/timetracker/static/timetracker/js/components/app.jsx
+++ web/timetracker/static/timetracker/js/components/app.jsx
@@ -1,8 +1,26 @@
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {taskDescription: ''};
+
+    this.changeTaskDescription = this.changeTaskDescription.bind(this);
+  }
+
   render() {
     return (
-      <h2>myApp</h2>
+      <div className="row" id="app">
+        <div className="col m8">
+          <input type="text" tabindex="1" value={this.state.taskDescription} onChange={this.changeTaskDescription} />
+        </div>
+        <div className="col m4">Uhr</div>
+      </div>
     )
+  }
+
+  changeTaskDescription(event) {
+    this.setState({taskDescription: event.target.value});
   }
 }
 
