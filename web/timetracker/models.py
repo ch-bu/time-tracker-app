f0cfafd1acee2eafedd088e0624796a8fcfbaf80
Christian Burkhart <burkhart.christian@gmail.com>
2017-03-01 19:04:56 +0100

    chore: rename Work model to Task model

--- web/timetracker/models.py
+++ web/timetracker/models.py
@@ -3,11 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Work(models.Model):
-    work_category = models.CharField(max_length=300)
-    work_type = models.CharField(max_length=300)
+class Task(models.Model):
+    # work_category = models.CharField(max_length=300)
+    # work_type = models.CharField(max_length=300)
     started = models.DateTimeField()
     stopped = models.DateTimeField()
+    duration = models.PositiveSmallIntegerField(default=0)
     goal = models.TextField()
-    comment = models.TextField()
-    feeling = models.PositiveSmallIntegerField()
+    # comment = models.TextField()
+    # feeling = models.PositiveSmallIntegerField()
--- web/timetracker/views.py
+++ web/timetracker/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse, JsonResponse
 from htmlmin.decorators import minified_response
-from timetracker.models import Work
+from timetracker.models import Task
 from django.contrib.auth.decorators import login_required
 
 @minified_response
