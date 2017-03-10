33982b10363f22879281aeb7a46e8cc39ac933b3
Christian Burkhart <burkhart.christian@gmail.com>
2017-03-10 20:24:49 +0100

    feat: Init single task api for deletion
    When a user click a task the task should
    be deleted. I started writing a class-based
    apiView with a delete request. the
    serializer backend is not working at the moment.
    I'll have to check that again.

--- web/serializers/urls.py
+++ web/serializers/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^tasks/', views.TaskView.as_view(), name="taskView"),
+    url(r'^task/(\d+)/$', views.TaskView.as_view(), name="taskView"),
+    url(r'^tasks/', views.TasksView.as_view(), name="tasksView"),
     url(r'^$', views.Test.as_view(), name='index')
 ]
--- web/serializers/views.py
+++ web/serializers/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
 from htmlmin.decorators import minified_response
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,10 +9,49 @@ from django.contrib.auth.models import User
 
 class Test(APIView):
 
-    def get(self, request, format=None):
-        return Response('tests')
+    def get(self, request, id, format=None):
+
+        task = Taks.objects.get(id=id)
+
+        serializer = TaskSerializer(task)
+
+        return JsonResponse({status: 'success'})
 
 class TaskView(APIView):
+
+    def getTask(self, id):
+        try:
+            task = Task.objects.get(id=id)
+        except ObjectDoesNotExist:
+            task = None
+
+        return task
+
+    def get(self, request, id, format=None):
+
+        task = self.getTask(id)
+
+        if not task:
+            return JsonResponse({'status': 'failed'}, status=400, safe=False)
+
+        serializer = TaskSerializer(task)
+
+        return JsonResponse({'status': 'success',
+            'data': serializer.data}, status=200, safe=False)
+
+    def delete(self, request, id, format=None):
+
+        task = self.getTask(id)
+
+        if not task:
+            return JsonResponse({'status': 'failed'}, status=400, safe=False)
+
+        task.delete()
+
+        return JsonResponse({'status': 'deleted task'}, status=200, safe=False)
+
+
+class TasksView(APIView):
 
     def get(self, request):
         # Get all tasks from user
