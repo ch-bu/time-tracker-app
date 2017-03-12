538433947e3ad0f01259b0c94e9ed9ab2d8f1cc7
Christian Burkhart <burkhart.christian@gmail.com>
2017-03-12 17:14:31 +0100

    feat: Generate chip after work and type of task blurs

--- web/timetracker/static/timetracker/js/components/app/input.jsx
+++ web/timetracker/static/timetracker/js/components/app/input.jsx
@@ -8,17 +8,51 @@ class Input extends React.Component {
     this.changeWorkDescription = this.changeWorkDescription.bind(this);
     this.changeTypeDescription = this.changeTypeDescription.bind(this);
     this.onButtonClicked = this.onButtonClicked.bind(this);
+    this.onBlurWork = this.onBlurWork.bind(this);
+    this.onBlurType = this.onBlurType.bind(this);
     this.tick = this.tick.bind(this);
+    this.onWorkChipClick = this.onWorkChipClick.bind(this);
+    this.onTypeChipClicked = this.onTypeChipClicked.bind(this);
 
     // Init state variables
     this.state = {buttonStart: true};
   }
 
+  componentWillMount() {
+    this.setState({workFocused: true,
+                   typeFocused: true});
+  }
+
   render() {
     // Get right icon (play vs. stop)
     var buttonText = this.state.buttonStart ? 'play_circle_filled' : 'stop';
-
     const task = this.props.taskDescription;
+
+    // Check if work should be a chip
+    var work;
+    var type;
+    if (this.state.workFocused == true) {
+      work =  <input tabindex="2" placeholder={"Arbeit"}
+                value={this.props.workDescription}
+                onChange={this.changeWorkDescription}
+                onBlur={this.onBlurWork}/>;
+    } else {
+        work = <div className="chip"
+          onClick={this.onWorkChipClick}>
+          {this.props.workDescription}</div>;
+    }
+
+    if (this.state.typeFocused == true) {
+        type = <input tabindex="3" placeholder={"Typ"}
+          value={this.props.typeDescription}
+          onChange={this.changeTypeDescription}
+          onBlur={this.onBlurType} />;
+    } else {
+      type = <div className="chip"
+        onClick={this.onTypeChipClicked}>
+        {this.props.typeDescription}</div>;
+    }
+
     return (
       <div className="row" id="bar-task">
         <div className="col s4 m4 l8">
@@ -31,14 +65,10 @@ class Input extends React.Component {
         <div className="col s8 m8 l4">
           <div className="row" id="bar-info">
             <div className="col s3 m2 l3">
-              <input tabindex="2" placeholder={"Arbeit"}
-                value={this.props.workDescription}
-                onChange={this.changeWorkDescription} />
+              {work}
             </div>
             <div className="col s3 m2 l3">
-              <input tabindex="3" placeholder={"Typ"}
-                value={this.props.typeDescription}
-                onChange={this.changeTypeDescription} />
+              {type}
             </div>
             <div className="col s3 m4 l3" id="bar-clock">
               <span>{this.props.taskDuration.format('HH:mm:ss')}</span>
@@ -52,6 +82,22 @@ class Input extends React.Component {
         </div>
       </div>
     );
+  }
+
+  onWorkChipClick() {
+    this.setState({workFocused: true});
+  }
+
+  onTypeChipClicked() {
+    this.setState({typeFocused: true});
+  }
+
+  onBlurWork() {
+    this.setState({workFocused: false});
+  }
+
+  onBlurType() {
+    this.setState({typeFocused: false});
   }
 
   // Change description of task whenever
@@ -75,6 +121,7 @@ class Input extends React.Component {
     if (!this.state.buttonStart) {
       this.props.onStopButtonClicked();
       clearInterval(this.timer);
+      this.setState({typeFocused: true, workFocused: true});
     // Start button is clicked
     } else {
       this.props.onStartButtonClicked();
